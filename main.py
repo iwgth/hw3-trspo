cf8df7c6524359f9466b013e0a78b2b3599c040f
iwgth <114057948+iwgth@users.noreply.github.com>
2023-09-25 20:25:43 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import multiprocessing
+import time
+def collatz_steps(n):
+    steps = 0
+    while n != 1:
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n = 3 * n + 1
+        steps += 1
+    return steps
+def calculate_average_steps(numbers, num_threads):
+    pool = multiprocessing.Pool(processes=num_threads)
+    results = pool.map(collatz_steps, numbers)
+    pool.close()
+    pool.join()
+    return sum(results) / len(numbers)
+if __name__ == "__main__":
+    N = int(input("Введіть N (к-ть натуральних чисел): "))
+    num_threads = int(input("Введіть к-ть потоків: "))
+    numbers = list(range(1, N + 1))
+    start_time = time.time()
+    average_steps = calculate_average_steps(numbers, num_threads)
+    end_time = time.time()
+    print(f"Середня к-ть кроків: {average_steps}")
+    print(f"Час виконання: {end_time - start_time} с")
